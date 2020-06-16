1c026f7b741b147bd398ed56fe4e6b0b08219af4
Jimmy Lau <jimmy.kp.lau@pccw.com>
2020-06-16 15:59:27 +0800

    Update for all apis

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+app.get('/api', async function (request, response) {
+  response.send(JSON.parse(JSON.stringify({server: "running"})));
+});
+
+// Account data
 app.get('/api/accounts', function (request, response) {
   var id = request.query.id;
 
@@ -23,6 +28,35 @@ app.get('/api/accounts', function (request, response) {
     }
 
     response.send(JSON.parse(JSON.stringify(result)));
+  })
+});
+
+// Payment data
+app.get('/api/payhistory', function (request, response) {
+  var accountId = request.query.accountId;
+
+  fs.readFile('./data/paymentHistory.json', 'utf8', (err, data) => {
+    if (err) { throw err; }
+
+    var result;
+    var paymentData = JSON.parse(data);
+
+    if (accountId) {
+      result = paymentData.filter(x => x.accountId === parseInt(accountId));
+    } else {
+      result = paymentData;
+    }
+
+    response.send(JSON.parse(JSON.stringify(result)));
+  })
+});
+
+// Gadget data
+app.get('/api/settings', function (request, response) {
+  fs.readFile('./data/gadgetSettings.json', 'utf8', (err, data) => {
+    if (err) { throw err; }
+
+    response.send(JSON.parse(data));
   })
 });
 
