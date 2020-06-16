d2e0bbb2e976cb193f71a49b7ad3846979599acf
Jimmy Lau <jimmy.kp.lau@pccw.com>
2020-06-16 11:52:26 +0800

    Update first commit

--- server.js
+++ server.js
@@ -7,19 +7,20 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/api-endpoint', function(request, response) {
+app.get('/api', function(request, response) {
     var nameString = request.query.name;
     var historyString = request.query.history;
     
-      var jsonContent = { 
-            title: nameString,
-            imageUrl: "https://image.flaticon.com/icons/png/512/61/61456.png",
-            contact: [ {id: 12345, name: "King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"}],
-            show: true 
-        };
+    var jsonContent = { 
+        title: nameString,
+        imageUrl: "https://image.flaticon.com/icons/png/512/61/61456.png",
+        contact: [ 
+            {id: 12345, name: "King Lai"},
+        ],
+        show: true 
+    };
 
     response.send(JSON.parse(JSON.stringify(jsonContent)));
-    
 });
 
 
